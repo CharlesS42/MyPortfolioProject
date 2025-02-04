b23dddffa034c450aaedb70ab55c7595a88c2696
charlesFinalMS <charles.seg42@gmail.com>
2025-02-04 08:26:07 -0500

    intial config

---
+++ be-portfolio/src/main/java/org/charl/beportfolio/BePortfolioApplication.java
@@ -0,0 +1,13 @@
+package org.charl.beportfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BePortfolioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BePortfolioApplication.class, args);
+    }
+
+}
---
+++ be-portfolio/src/test/java/org/charl/beportfolio/BePortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package org.charl.beportfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BePortfolioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
